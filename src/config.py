0de9f99cd04fde591575983a6a4ba25e88f41df2
DaniilAndreyev <dandreyev.ldm@gmail.com>
2026-02-12 14:27:22 -0500

    fix: incorrect lib usage

--- src/config.py
+++ src/config.py
@@ -5,7 +5,7 @@ import os
 
 
 # directory paths
-REPO_DIR = os.path.dirname(os.path.dirname(os.abspath(__file__))) # ../../__file__
+REPO_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__))) # ../../__file__
 
 STIMULI_DIR = os.path.join(REPO_DIR, "stimuli")
 DATA_DIR = os.path.join(REPO_DIR, "data")
@@ -21,8 +21,8 @@ STORY_VIDEO_AUDIO = os.path.join(AUDIO_DIR, "video_story.wav")
 
 VIDEO_FILE = os.path.join(VIDEO_DIR, "video.mp4")
 
-QUIZ_BLACK_FILE = os.path(QUIZ_DIR, "black_quiz.csv")
-QUIZ_VIDEO_FILE = os.path(QUIZ_DIR, "video_quiz.csv")
+QUIZ_BLACK_FILE = os.path.join(QUIZ_DIR, "black_quiz.csv")
+QUIZ_VIDEO_FILE = os.path.join(QUIZ_DIR, "video_quiz.csv")
 
 
 # screen config
