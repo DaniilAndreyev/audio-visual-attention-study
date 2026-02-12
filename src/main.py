64fbe125f69f916fc383937dd101406f7b2a11bb
DaniilAndreyev <dandreyev.ldm@gmail.com>
2026-02-12 15:12:24 -0500

    feat: add helper functions and placeholder

--- src/main.py
+++ src/main.py
@@ -1,6 +1,16 @@
-from psychopy import visual
+from psychopy import visual, event
 import config
 
+# helper functions
+def safe_quit(win):
+    if win:
+        win.close()
+
+def check_for_quit(win):
+    if config.QUIT_KEY in event.getKeys():
+        safe_quit(win)
+
+# create window
 def create_window():
 	return visual.Window(
 		size = config.WINDOW_SIZE,
@@ -9,13 +19,24 @@ def create_window():
 		units = 'height',
 	)
 
+# placeholder for future
+def show_placeholder(win):
+    text = visual.TextStim(
+		win,
+		text = 'Press Esc to exit.',
+		color = config.TEXT_COLOR,
+		height = 0.03
+	)
+    
+    while True:
+        text.draw()
+        win.flip()
+        check_for_quit(win)
+
 def main():
     win = create_window()
     
-    while True:
-        win.flip()
-    
+    show_placeholder(win)
 
-
-if __name__ == "__main__":
+if __name__ == '__main__':
     main()
