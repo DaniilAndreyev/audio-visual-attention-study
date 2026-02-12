4f29c4e6b5f34c6266d7bd1a7e590435aa4219fd
DaniilAndreyev <dandreyev.ldm@gmail.com>
2026-02-12 15:46:50 -0500

    feat: main loop + sys exit

--- src/main.py
+++ src/main.py
@@ -1,33 +1,32 @@
 from psychopy import visual, event, gui
 import config
+import sys
 
 # helper functions
 def safe_quit(win):
     if win:
         win.close()
+    sys.exit()
 
 def check_for_quit(win):
     if config.QUIT_KEY in event.getKeys():
         safe_quit(win)
 
-# participant info
+
+# get participant info
 def get_participant_info():
-	while True:
-		info = {
-			'id': '',
-			'age': '',
-			'gender': ['Male', 'Female', 'Other']
-		}
+	info = {
+		'age': '',
+		'gender': ['Male', 'Female', 'Other']
+	}
 
-		dialog = gui.DlgFromDict(info, title='Participant Info')
+	dialog = gui.DlgFromDict(info, title='Participant Info')
 
-		if not dialog.OK:
-			return None
+	if not dialog.OK:
+		return None
 
-		if info['id'].strip():
-			return info
-		gui.Dlg(title='Error', labelButtonOK='OK').addText('Some fields are empty').show()
-	
+	return info
+
 
 # create window
 def create_window():
@@ -38,11 +37,12 @@ def create_window():
 		units = 'height',
 	)
 
+
 # placeholder for future
 def show_placeholder(win):
     text = visual.TextStim(
 		win,
-		text = 'Press Esc to exit.',
+		text = 'Experiment Running\nPress Esc to exit.',
 		color = config.TEXT_COLOR,
 		height = 0.03
 	)
@@ -52,21 +52,28 @@ def show_placeholder(win):
         win.flip()
         check_for_quit(win)
 
+
 def main():
-    
-    # Get participant info
-    participant_info = get_participant_info()
-    
-    if participant_info is None:
-        print('Experiment cancelled.')
-        return
-    
-    print(participant_info)
-    
-    # Create window
-    win = create_window()
-    
-    show_placeholder(win)
+
+	# Get participant info
+	participant_info = get_participant_info()
+
+	if participant_info is None:
+		print('Experiment cancelled.')
+		return
+
+	print(participant_info)
+
+	# Create window
+	win = create_window()
+
+
+	try:
+		show_placeholder(win)
+	except Exception as e:
+		print('Error: ', e)
+	finally:
+		safe_quit(win)
 
 if __name__ == '__main__':
     main()
