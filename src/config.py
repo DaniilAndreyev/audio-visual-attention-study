cd3dba30dccdefd89779ac083a0c9f74a0a750d0
DaniilAndreyev <dandreyev.ldm@gmail.com>
2026-02-12 13:45:08 -0500

    feat: dir paths, stimuli paths, screen configs

---
+++ src/config.py
@@ -0,0 +1,31 @@
+"""
+Config file that stores constants for the project
+"""
+import os
+
+# directory paths
+REPO_DIR = os.path.dirname(os.path.dirname(os.abspath(__file__))) # ../../__file__
+
+STIMULI_DIR = os.path.join(REPO_DIR, "stimuli")
+
+AUDIO_DIR = os.path.join(STIMULI_DIR, "audio")
+VIDEO_DIR = os.path.join(STIMULI_DIR, "video")
+QUIZ_DIR = os.path.join(STIMULI_DIR,)
+
+
+# stimuli files
+STORY_BLACK_AUDIO = os.path.join(AUDIO_DIR, "black_story.wav")
+STORY_VIDEO_AUDIO = os.path.join(AUDIO_DIR, "video_story.wav")
+
+VIDEO_FILE = os.path.join(VIDEO_DIR, "video.mp4")
+
+QUIZ_BLACK_FILE = os.path(QUIZ_DIR, "black_quiz.csv")
+QUIZ_VIDEO_FILE = os.path(QUIZ_DIR, "video_quiz.csv")
+
+# screen config
+WINDOW_SIZE = (1920, 1080)
+FULLSCREEN = True
+BACKGROUND_COLOR = "black"
+TEXT_COLOR = "white"
+
+FRAME_RATE = 60
