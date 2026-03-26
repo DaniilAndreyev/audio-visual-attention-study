d4f74e3f653ed001a1c3353184d58f399d49e916
DaniilAndreyev <dandreyev.ldm@gmail.com>
2026-03-26 12:17:17 -0400

    fix: indentation error

--- src/stimuli.py
+++ src/stimuli.py
@@ -8,18 +8,18 @@ import utils
 
 def play_audio(win, audio_file):
 	win.color = "black"
-    win.flip()
+	win.flip()
 
-    story = sound.Sound(audio_file)
-    story.play()
+	story = sound.Sound(audio_file)
+	story.play()
 
-    clock = core.Clock()
+	clock = core.Clock()
 
-    while clock.getTime() < story.getDuration():
-        utils.check_for_quit(win)
-        core.wait(0.01)
+	while clock.getTime() < story.getDuration():
+		utils.check_for_quit(win)
+		core.wait(0.01)
 
-    return True
+	return True
 
 
 def play_video_with_audio(win, video_file, audio_file):
